d91cb7e759b62ebdafcafd5b57a2044888081f00
samsjchi <samuel.sj.chi@gmail.com>
2018-03-14 19:56:18 -0400

    Add Behanced logo

---
+++ app/components/Logo.js
@@ -0,0 +1,12 @@
+var React = require('react')
+import BehancedLogo from '../assets/behanced-logo.svg'
+
+function Logo () {
+  return (
+    <div className='logo'>
+      <BehancedLogo width={250} />
+    </div>
+  )
+}
+
+module.exports = Logo
