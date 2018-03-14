3fbd4498e13cab92000b6f60581be2e3d252b022
samsjchi <samuel.sj.chi@gmail.com>
2018-03-14 15:34:34 -0400

    Add CORS headers to devServer

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,12 @@ module.exports = {
     new webpack.DefinePlugin({
       API_KEY: JSON.stringify(myEnv.parsed.API_KEY),
     })
-  ]
+  ],
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  }
 }
