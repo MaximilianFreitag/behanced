4f6cf1311699b07a5f5f124097f73672f6dfb751
samsjchi <samuel.sj.chi@gmail.com>
2018-03-14 16:43:56 -0400

    Create ProjectGridItem stateless functional component

--- app/components/ProjectGrid.js
+++ app/components/ProjectGrid.js
@@ -1,29 +1,16 @@
 var React = require('react')
 var PropTypes = require('prop-types')
+var ProjectGridItem = require('./ProjectGridItem')
 
-function ProjectGrid (props) {
+function ProjectGrid ({ projects }) {
   return (
     <ul className='project-grid'>
-      {props.projects.map(function (project) {
+      {projects.map(function (project) {
         return (
-          <li className='project-card' key={project.id}>
-            <img
-              className='project-card__image'
-              src={project.covers['202']}
-              alt={'Project cover for ' + project.name} />
-
-            <div className='project-card__details'>
-              <h3><a href={project.url}>{project.name}</a></h3>
-              <p>By: <a href={project.owners[0].url}>{project.owners[0].first_name} {project.owners[0].last_name}</a></p>
-              <p>Field: {project.fields[0]}</p>
-            </div>
-
-            <ul className='project-card__stats'>
-              <li>{project.stats.appreciations} appreciations</li>
-              <li>{project.stats.views} views</li>
-              <li>{project.stats.comments} comments</li>
-            </ul>
-          </li>
+          <ProjectGridItem
+            project={project}
+            key={project.id}
+          />
         )
       })}
     </ul>
---
+++ app/components/ProjectGridItem.js
@@ -0,0 +1,31 @@
+var React = require('react')
+var PropTypes = require('prop-types')
+
+function ProjectGridItem ({ project }) {
+  return (
+    <li className='project-card' key={project.id}>
+      <img
+        className='project-card__image'
+        src={project.covers['202']}
+        alt={'Project cover for ' + project.name} />
+
+      <div className='project-card__details'>
+        <h3><a href={project.url}>{project.name}</a></h3>
+        <p>By: <a href={project.owners[0].url}>{project.owners[0].first_name} {project.owners[0].last_name}</a></p>
+        <p>Field: {project.fields[0]}</p>
+      </div>
+
+      <ul className='project-card__stats'>
+        <li>{project.stats.appreciations} appreciations</li>
+        <li>{project.stats.views} views</li>
+        <li>{project.stats.comments} comments</li>
+      </ul>
+    </li>
+  )
+}
+
+ProjectGridItem.propTypes = {
+  project: PropTypes.object.isRequired
+}
+
+module.exports = ProjectGridItem
