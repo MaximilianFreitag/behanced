8563d630ca7803f0b3595cd615abb402137fdc11
samsjchi <samuel.sj.chi@gmail.com>
2018-03-14 16:37:20 -0400

    Add additional fields

--- app/components/FieldMenu.js
+++ app/components/FieldMenu.js
@@ -2,7 +2,7 @@ var React = require('react')
 var PropTypes = require('prop-types')
 
 function FieldMenu (props) {
-  var fields = ['Architecture', 'Art Direction', 'Graphic Design', 'Illustration', 'Photography', 'UI/UX', 'Web Design']
+  var fields = ['Architecture', 'Art Direction', 'Branding', 'Fashion', 'Graphic Design', 'Illustration', 'Photography', 'UI/UX', 'Web Design']
 
   return (
     <ul className='field-menu'>
