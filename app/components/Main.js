e49c253064461cfc9e6db8505cc14f337606f7b5
samsjchi <samuel.sj.chi@gmail.com>
2018-03-14 13:32:34 -0400

    Add state and update selectedField when user clicks on a field

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,16 +1,39 @@
 var React = require('react')
 
 class Main extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      selectedField: 'All'
+    }
+
+    this.updateField = this.updateField.bind(this)
+  }
+
+  updateField (field) {
+    this.setState(function () {
+      return {
+        selectedField: field
+      }
+    })
+  }
+
   render () {
-    var fields = ['All', 'Architecture', 'Art Direction', 'Graphic Design', 'Illlustration', 'Photography', 'UI/UX', 'Web Design'];
+    var fields = ['All', 'Architecture', 'Art Direction', 'Graphic Design', 'Illlustration', 'Photography', 'UI/UX', 'Web Design']
 
     return (
       <ul className='fields'>
         {fields.map(function (field) {
           return (
-            <li key={field}>{field}</li>
+            <li
+              style={field === this.state.selectedField ? { color: '#ff3530' } : null}
+              onClick={this.updateField.bind(null, field)}
+              key={field}>
+              {field}
+            </li>
           )
-        })}
+        }, this)}
       </ul>
     )
   }
