40a35e29edf0b7a66441b4431e9adf021b7f21a2
samsjchi <samuel.sj.chi@gmail.com>
2018-03-15 16:00:55 -0400

    Create API call to fetch creative fields

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,11 +2,19 @@ var axios = require('axios')
 
 module.exports = {
   fetchProjects: function (field) {
-    var encodedURI = window.encodeURI('http://www.behance.net/v2/projects?client_id=' + API_KEY + '&sort=appreciations&field=' + field)
+    var encodedURI = window.encodeURI('https://api.behance.net/v2/projects?client_id=' + API_KEY + '&sort=appreciations&field=' + field)
 
     return axios.get(encodedURI)
       .then(function (response) {
         return response.data.projects
+      })
+  },
+  fetchFields: function () {
+    var encodedURI = window.encodeURI('https://api.behance.net/v2/fields?client_id=' + API_KEY)
+
+    return axios.get(encodedURI)
+      .then(function (response) {
+        return response.data.fields
       })
   }
 }
