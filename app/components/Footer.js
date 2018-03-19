e5f0ef000e4b6898ef20b6439ac62cf516e61068
samsjchi <samuel.sj.chi@gmail.com>
2018-03-19 17:43:42 -0400

    Create Footer component

---
+++ app/components/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Footer () {
+  return (
+    <footer className='footer'>
+      <div className='footer__behanced-logo'>
+        BEHANCED LOGO HERE
+      </div>
+
+      <div className='footer__tech-container'>
+        <div className='footer__adobe-behance'>
+          <div className='footer__adobe-logo'>
+            Adobe Logo
+          </div>
+          <div className='footer__powered-by-behance'>
+            Powered by Behance Logo
+          </div>
+        </div>
+
+        <div className='footer__web-tech'>
+          <p>...and the following web technologies:</p>
+          <ul className='footer__web-tech-list'>
+            <li>React</li>
+            <li>Babel</li>
+            <li>webpack</li>
+            <li>Sass</li>
+            <li>Firebase</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className='footer__github-link'>
+        VIEW ON GITHUB
+      </div>
+    </footer>
+  )
+}
