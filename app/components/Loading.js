df20f79627f01e9b176cef9eda33deee3bb6c9f9
samsjchi <samuel.sj.chi@gmail.com>
2018-03-19 16:09:05 -0400

    Create Loading component

---
+++ app/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Loading () {
+  return (
+    <div className='spinner'>
+      <div className='bounce1 bounce'></div>
+      <div className='bounce2 bounce'></div>
+      <div className='bounce3 bounce'></div>
+    </div>
+  )
+}
