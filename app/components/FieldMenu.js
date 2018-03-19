d5bc8cc24f6ceebf1fc7615ec5d2b590dd75a8aa
samsjchi <samuel.sj.chi@gmail.com>
2018-03-19 00:39:25 -0400

    Refactor FieldMenu.js to ES6

--- app/components/FieldMenu.js
+++ app/components/FieldMenu.js
@@ -2,20 +2,28 @@ var React = require('react')
 var PropTypes = require('prop-types')
 var FieldMenuItem = require('./FieldMenuItem')
 
-function FieldMenu (props) {
-  var fields = ['Architecture', 'Art Direction', 'Branding', 'Fashion', 'Graphic Design', 'Illustration', 'Photography', 'UI/UX', 'Web Design']
+function FieldMenu ({ selectedField, onSelect }) {
+  const fields = [
+    'Architecture',
+    'Art Direction',
+    'Branding',
+    'Fashion',
+    'Graphic Design',
+    'Illustration',
+    'Photography',
+    'UI/UX',
+    'Web Design'
+  ]
 
   return (
     <ul className='field-menu'>
-      {fields.map(function (field) {
-        return (
-          <FieldMenuItem
-            field={field}
-            selectedField={props.selectedField}
-            onSelect={props.onSelect}
-          />
-        )
-      })}
+      {fields.map((field) => (
+        <FieldMenuItem
+          field={field}
+          selectedField={selectedField}
+          onSelect={onSelect}
+        />
+      ))}
     </ul>
   )
 }
