eb822291263a4d8391e38d8d0fd1903c15c92c00
samsjchi <samuel.sj.chi@gmail.com>
2018-03-19 17:44:06 -0400

    Render Footer component in App.js

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
 import Main from './Main'
+import Footer from './Footer'
 
 class App extends Component {
   render () {
     return (
       <div className='container'>
         <Main />
+        <Footer />
       </div>
     )
   }
